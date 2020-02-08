b5c32e56dd34ae824f330dae42800dd557d41f33
Julio Cesar da Silva Oliveira <1196702575@acd.grupotiradentes.com>
2020-02-08 11:17:06 -0300

    Inclusão Insertion e Selection + Inicio Main

--- src/aplicacao/Main.java
+++ src/aplicacao/Main.java
@@ -1,26 +1,79 @@
 package aplicacao;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 import implementacao.Pilha;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+
+		Scanner teclado = new Scanner(System.in);
+
 		Pilha navio = new Pilha();
 
-		System.out.println(navio.push("Chile"));
-		System.out.println(navio.push("Sudao"));
-		System.out.println(navio.push("Egito"));
-		System.out.println(navio.push("India"));
-		System.out.println(navio.push("China"));
-		navio.listar();
-		System.out.println(navio.cheio());
-		System.out.println(navio.vazio());
-		System.out.println(navio.top());
-		System.out.println(navio.pop());
+		int loopAplicacao = 1;
 
-		navio.listar();
-		
+		while (loopAplicacao != 0) {
+			System.out.println("1 - Listar Pilha");
+			System.out.println("2 - Empilhar");
+			System.out.println("3 - Desempilhar");
+			System.out.println("4 - Verificar Topo");
+			System.out.println("5 - Vazia");
+			System.out.println("6 - Cheia");
+			System.out.println("7 - Listar Insertion Sort");
+			System.out.println("8 - Listar Selection Sort");
+			System.out.println("0 - SAIR");
+			int opcaoMenu = teclado.nextInt();
+			switch (opcaoMenu) {
+			case 1:
+				navio.listar();
+				break;
+			case 2:
+				System.out.println(navio.push(55));// Brasil
+				System.out.println(navio.push(54));// Argentina
+				System.out.println(navio.push(56));// Chile
+				System.out.println(navio.push(57));// Colômbia
+				System.out.println(navio.push(598));// Uruguai
+				System.out.println(navio.push(595));// Paraguai
+				System.out.println(navio.push(51));// Peru
+				System.out.println(navio.push(591));// Bolivia
+				System.out.println(navio.push(593));// Equador
+				break;
+			case 3:
+				System.out.println(navio.pop());
+				break;
+			case 4:
+				System.out.println(navio.top());
+				break;
+			case 5:
+				System.out.println(navio.vazio());
+				break;
+			case 6:
+				System.out.println(navio.cheio());
+				break;
+			case 7:
+				navio.listarInsertion();
+				break;
+			case 8:
+				navio.listarSelection();
+				break;
+			case 0:
+				System.out.println("Sistema Encerrado!");
+				loopAplicacao = 0;
+				break;
+			default:
+				System.out.println("Valor Inválido.");
+				break;
+			}
+
+		}
+
+		teclado.close();
 
 	}
 
--- src/implementacao/Pilha.java
+++ src/implementacao/Pilha.java
@@ -3,9 +3,9 @@ package implementacao;
 public class Pilha {
 
 	private int topo = -1;
-	final int TAMANHO = 5;
+	final int TAMANHO = 8;
 	
-	String[] conteiner = new String[TAMANHO];
+	int[] americaSul = new int[TAMANHO];
 	
 	public String vazio() {
 		if(topo == -1) {
@@ -23,15 +23,15 @@ public class Pilha {
 	
 	public String top() {
 		if(topo > -1) {
-			return "O Conteiner " + conteiner[topo] + " está no Topo!";
+			return "O Conteiner " + americaSul[topo] + " está no Topo!";
 		}
 		return "Não há Contêiner(s) no Navio!";
 	}
 	
-	public String push(String elemento) {
+	public String push(int elemento) {
 		if(topo < TAMANHO - 1) {
 			topo++;
-			conteiner[topo] = elemento;
+			americaSul[topo] = elemento;
 			return "Conteiner " + elemento + " adicionado com Sucesso!";
 		}
 		return "Navio lotado de Conteiners - Não foi possível adicionar " + elemento + "!";
@@ -42,25 +42,63 @@ public class Pilha {
 			return "Não há Contêiner(s) no Navio!";
 		}
 		if(topo == 0) {
-			String auxiliar = conteiner[0];
-			conteiner[0] = null;
+			int auxiliar = americaSul[0];
+			americaSul[0] = 0;
 			topo--;
 			return "Conteiner " + auxiliar + " removido do navio com Sucesso!";
 		}
-		String auxiliar = conteiner[topo]; 
-		conteiner[topo] = null;
+		int auxiliar = americaSul[topo]; 
+		americaSul[topo] = 0;
 		topo--;
 		return "Conteiner " + auxiliar + " removido do navio com Sucesso";
 	}
 	
 	public void listar() {
 		for(int i = TAMANHO - 1; i >= 0; i--) {
-			if(conteiner[i] != null) {
-				System.out.println("--------");
-				System.out.println("| " + conteiner[i] + " |");
-				System.out.println("--------");
+			if(americaSul[i] != 0) {
+				System.out.println("|" + americaSul[i] + "|\n");
 			}
 		}
 	}
+	
+	public void listarInsertion() {
+		for(int i = 1; i < americaSul.length; i++) {
+			int j = i - 1;
+			int key = americaSul[i];
+			
+			while((j >= 0) && (americaSul[j] >= key)) {
+				americaSul[j + 1] = americaSul[j];
+				j--;
+			}
+			americaSul[j + 1] = key;
+		}
+		
+		System.out.println("Contêiner América do Sul - Listado Ordenado Insertion Sort: ");
+		for(int i = 0; i < americaSul.length; i++) {
+			System.out.println(americaSul[i]);
+		}
+	}
+	
+	public void listarSelection() {
+		int indice = 0, menorNumero = 0;
+		
+		for(int i = 0; i < americaSul.length - 1; i++) {
+			indice = i;
+			for(int j = i + 1; j < americaSul.length; j++) {
+				if(americaSul[j] < americaSul[indice]) {
+					indice = j;
+				}
+			}
+			menorNumero = americaSul[indice];
+			americaSul[indice] = americaSul[i];
+			americaSul[i] = menorNumero;
+		}
+		
+		System.out.println("Contêiner América do Sul - Listado Ordenado Selection Sort: ");
+		for(int i = 0; i < americaSul.length; i++) {
+			System.out.println(americaSul[i]);
+		}
+	}
+	
 	
 }
