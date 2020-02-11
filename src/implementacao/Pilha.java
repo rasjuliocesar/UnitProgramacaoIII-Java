bdb0603369e047cb7047dd03e9beb92299d4b05f
Julio Cesar <rasjuliocesar@gmail.com>
2020-02-11 07:14:45 -0300

    Clone de vetor.

--- src/implementacao/Pilha.java
+++ src/implementacao/Pilha.java
@@ -6,7 +6,7 @@ public class Pilha {
 	final int TAMANHO = 8;
 
 	String[] americaSul = new String[TAMANHO];
-
+	
 	public boolean vazio() {
 		if (topo == -1) {
 			return true;
@@ -66,14 +66,15 @@ public class Pilha {
 
 	public void listarBubbleSort() {
 		String aux = null;
+		String[] listar = americaSul.clone();
 
 		for (int i = 0; i <= americaSul.length - 1; i++) {
 			for (int j = 1; j <= americaSul.length - 1 - i; j++) {
-				if (americaSul[j] != null) {
-					if (americaSul[j - 1].charAt(0) > americaSul[j].charAt(0)) {
-						aux = americaSul[j - 1];
-						americaSul[j - 1] = americaSul[j];
-						americaSul[j] = aux;
+				if (listar[j] != null) {
+					if (listar[j - 1].charAt(0) > listar[j].charAt(0)) {
+						aux = listar[j - 1];
+						listar[j - 1] = listar[j];
+						listar[j] = aux;
 					}
 				}
 			}
@@ -82,66 +83,69 @@ public class Pilha {
 		System.out.println("\nCotêiner América do Sul - Listado Ordenado Bubble Sort: \n");
 
 		for (int i = 0; i <= americaSul.length - 1; i++) {
-			if (americaSul[i] != null) {
-				System.out.println(americaSul[i]);
+			if (listar[i] != null) {
+				System.out.println(listar[i]);
 			}
 		}
 	}
 
 	public void listarInsertion() {
+		String[] listar = americaSul.clone();
+		
 		for (int i = 1; i < americaSul.length; i++) {
 
 			int j = i - 1;
-			String key = americaSul[i];
+			String key = listar[i];
 
-			if (americaSul[i] != null) {
-				char elementoUm = americaSul[i].charAt(0);
-				char elementoDois = americaSul[j].charAt(0);
+			if (listar[i] != null) {
+				char elementoUm = listar[i].charAt(0);
+				char elementoDois = listar[j].charAt(0);
 
 				while ((j >= 0) && (elementoDois >= elementoUm)) {
-					americaSul[j + 1] = americaSul[j];
+					listar[j + 1] = listar[j];
 					j--;
 					if (j >= 0) {
-						elementoDois = americaSul[j].charAt(0);
+						elementoDois = listar[j].charAt(0);
 					}
 				}
-				americaSul[j + 1] = key;
+				listar[j + 1] = key;
 			}
 		}
 
 		System.out.println("\nContêiner América do Sul - Listado Ordenado Insertion Sort: \n");
 
 		for (int i = 0; i < americaSul.length; i++) {
-			if (americaSul[i] != null) {
-				System.out.println(americaSul[i]);
+			if (listar[i] != null) {
+				System.out.println(listar[i]);
 			}
 		}
 	}
 
 	public void listarSelection() {
 		int indice = 0;
+		String[] listar = americaSul.clone();
 		String menorNumero = null;
 
 		for (int i = 0; i < americaSul.length - 1; i++) {
 			indice = i;
 
 			for (int j = i + 1; j < americaSul.length; j++) {
-				if (americaSul[j] != null) {
-					if (americaSul[j].charAt(0) < americaSul[indice].charAt(0)) {
+				if (listar[j] != null) {
+					if (listar[j].charAt(0) < listar[indice].charAt(0)) {
 						indice = j;
 					}
 				}
 			}
-			menorNumero = americaSul[indice];
-			americaSul[indice] = americaSul[i];
-			americaSul[i] = menorNumero;
+			menorNumero = listar[indice];
+			listar[indice] = listar[i];
+			listar[i] = menorNumero;
 		}
 
 		System.out.println("\nContêiner América do Sul - Listado Ordenado Selection Sort: \n");
 
 		for (int i = 0; i < americaSul.length; i++) {
-			if (americaSul[i] != null) {
-				System.out.println(americaSul[i]);
+			if (listar[i] != null) {
+				System.out.println(listar[i]);
 			}
 		}
 	}
